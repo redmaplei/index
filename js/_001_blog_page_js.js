e6c40d8ffde4a07629d3b2fee7ac594ed85762a3
-001 <1219288694@qq.com>
2018-11-10 18:26:51 +0800

    index init

--- js/_001_blog_page_js.js
+++ js/_001_blog_page_js.js
@@ -1,32 +1,5 @@
-
-
 $(function () {
 
-    // var url = "http://127.0.0.1:8090/markdown/getAllMarkDownVO"
-    // var url = "http://www.redwys.xyz/markdown/getAllMarkDownVO";
-    //var blogtitle = "tomcat的使用";
-    //var url = "http://localhost:8080/markdown/getOneBlogToTitle?blogTitle="+blogtitle;
-
-    // $.ajax({
-    //     url:url,
-    //     type:'GET',
-    //     dataType:'jsonp',
-    //     contentType:'application/json; charset=UTF-8',
-    //     success:function (data) {
-
-    //         console.log(3443);
-    //         console.log(data);
-    //       //  wenzan_vue.wenzans = data;
-
-    //        // console.log("sd"+wenzan_vue.wenzans);
-    //     },
-    //     error: function() {
-    //         console.log("xxxxx");
-    //     }
-
-    // });
-
-    // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     var urlread = "http://127.0.0.1:8080/md/getnewmd";
 
     $.ajax({
@@ -45,56 +18,6 @@ $(function () {
         }
 
     });
-
-
-    // var fids = "sdfsd";
-    // var encrypt = "true";
-    // var expireTime = 7;
-
-    // $.ajax({
-    //     url: "http://192.168.2.86:8080/share/create",
-    //     dataType: 'json',
-    //     type: 'post',
-    //     data:{
-    //     		"fids": fids,"encrypt": encrypt,"expireTime": expireTime
-    //     },
-    //     beforeSend: function (xhr) {
-    //         xhr.setRequestHeader("Test", "testheadervalue");
-
-    //     },
-    //     async: false,
-    //     cache: false,
-    //     //contentType: 'application/x-www-form-urlencoded',
-    //     success: function (sResponse) {
-
-    //         console.log(sResponse);
-
-    //     }
         
-
-    // });
-
-    // var urlpan = "http://192.168.2.239:8080/disk/files/get?page=1&limit=10&pid=1";
-
-    // $.ajax({
-    //     url:urlpan,
-    //     type:'GET',
-    //     dataType:'json',
-    //     contentType:'application/json; charset=UTF-8',
-    //     success:function (data) {
-
-    //         console.log(data);
-
-    //     },
-        // error: function() {
-        //     console.log("xsss");
-        // }
-
-    // });
-
-
 });
-
- 
-
 
--- js/blogread.js
+++ js/blogread.js
@@ -1,4 +1,3 @@
-
 $(function () {
 
 
@@ -7,12 +6,28 @@ $(function () {
 
     var bloginfourl = "http://localhost:8080/md/getmd?blogtitle="+blogtitle;
     // var bloginfourl = "http://www.redwys.xyz/markdown/getOneMarkDownVO?blogTitle="+blogtitle;
+
+    $.ajax({
+        url:bloginfourl,
+        type:'GET',
+        dataType:'json',
+        contentType:'application/json; charset=UTF-8',
+        success:function (data) {
+            console.log(32);
+            console.log(data);
+        }
+
+    });
+
+
     $.ajax({
         url:bloginfourl,
         type:'GET',
         dataType:"json",
         contentType:'application/json; charset=UTF-8',
         success:function (data) {
+            alert(23);
+            console.log("16" + data);
             blog_read_info_vue.article = data;
             var content = blog_read_info_vue.article.mdContent;
             // $("#read_textera").val(content);
@@ -35,7 +50,7 @@ $(function () {
     });
 
     editormd.markdownToHTML("blog_read_editormd", {
-                htmlDecode: "style,script,iframe", //可以过滤标签解码
+                htmlDecode: "style,script,iframe", // 可以过滤标签解码
                 emoji: true,
                 taskList:true,
                 tex: true,               // 默认不解析
@@ -46,20 +61,6 @@ $(function () {
                 theme: "gray",
                 previewTheme: "dark",
                 path :  '../editormd/lib/'
-
-                // width        : "90%",
-                // height       : 720,
-                
-                // // Editor.md theme, default or dark, change at v1.5.0
-                // // You can also custom css class .editormd-preview-theme-xxxx
-                // theme        :  "dark",
-                
-                // // Preview container theme, added v1.5.0
-                // // You can also custom css class .editormd-preview-theme-xxxx
-                // previewTheme :  "dark", 
-                
-                // // Added @v1.5.0 & after version is CodeMirror (editor area) theme
-                // editorTheme  :  "pastel-on-dark", 
     });
 
 });
