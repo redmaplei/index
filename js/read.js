391e2a69a6408a6718ee2745892168f93365c4aa
-001 <1219288694@qq.com>
2018-11-11 00:26:25 +0800

    小修改

--- js/read.js
+++ js/read.js
@@ -2,13 +2,9 @@ $(function () {
 
 
     var blogtitle = localStorage.getItem("blogtitle");
-    // alert("blogtitle:xxxx"+blogtitle);
 
     var bloginfourl = "http://localhost:8080/md/getmd?blogtitle="+blogtitle;
     // var bloginfourl = "http://www.redwys.xyz/markdown/getOneMarkDownVO?blogTitle="+blogtitle;
-
-    var urlread = "http://127.0.0.1:8080/md/getnewmd";
-
 
     $.ajax({
         url:bloginfourl,
@@ -21,7 +17,7 @@ $(function () {
             
             blog_read_info_vue.article = data;
 
-            document.getElementById('content').innerHTML = marked(blog_read_info_vue.article.markdowninfo);
+            document.getElementById('content').innerHTML = marked(blog_read_info_vue.article.mdContent);
 
         },
         error: function() {
