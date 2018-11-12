c74419720bd26b9c90bf7a792178988deec76462
-001 <1219288694@qq.com>
2018-11-12 16:52:54 +0800

    登陆和文章操作没完成

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,52 +1,52 @@
 
- var urlread = "http://127.0.0.1:8080/md/getnewmd";
+//  var urlread = "http://127.0.0.1:8080/md/getnewmd";
 
- $.ajax({
-     url:urlread,
-     type:'GET',
-     dataType:'json',
-     contentType:'application/json; charset=UTF-8',
-     success:function (data) {
+//  $.ajax({
+//      url:urlread,
+//      type:'GET',
+//      dataType:'json',
+//      contentType:'application/json; charset=UTF-8',
+//      success:function (data) {
 
-         console.log(data);
-         console.log(2121);
-         mdvue.articles = data;
+//          console.log(data);
+//          console.log(2121);
+//          mdvue.articles = data;
          
-         console.log(2121);
-         // console.log("sd"+wenzan_vue.wenzans);
-     },
-     error: function() {
-         console.log("xsss");
-     }
+//          console.log(2121);
+//          // console.log("sd"+wenzan_vue.wenzans);
+//      },
+//      error: function() {
+//          console.log("xsss");
+//      }
 
- });
+//  });
 
-// function logout1() {
-//     // layer.open({
+// // function logout1() {
+// //     // layer.open({
 
-//     //     title:"退出",
-//     // });
-//     layer.alert("确认退出吗", {
-//         yes: function() {
+// //     //     title:"退出",
+// //     // });
+// //     layer.alert("确认退出吗", {
+// //         yes: function() {
+// //             layer.msg("退出成功");
+// //             location.reload();
+// //         }
+        
+// //     });
+// // }
+
+// function logout() {
+
+//     layer.open({
+//         title:"退出",
+//         btn: ['确定', '取消'],
+//         content: "确认退出吗?",
+//         btn1: function() {
 //             layer.msg("退出成功");
 //             location.reload();
+//         },
+//         btn2: function() {
+
 //         }
-        
-//     });
+//     })
 // }
-
-function logout() {
-
-    layer.open({
-        title:"退出",
-        btn: ['确定', '取消'],
-        content: "确认退出吗?",
-        btn1: function() {
-            layer.msg("退出成功");
-            location.reload();
-        },
-        btn2: function() {
-
-        }
-    })
-}
